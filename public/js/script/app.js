2425447ba2cce8f3a334797b365bed5b2859c980
sanjib007 <loton1984@gmail.com>
2018-03-22 23:52:57 +0600

    some done

--- public/js/script/app.js
+++ public/js/script/app.js
@@ -1,21 +1,92 @@
 // Define the `phonecatApp` module
-var tms = angular.module('tms', []);
+var tms = angular.module('tms', [], function ($interpolateProvider) {
+
+});
+tms.config(function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('<%');
+    $interpolateProvider.endSymbol('%>');
+});
+
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 tms.controller('temsSetupController', function temsSetupController($scope) {
-
-    $scope.userVM = {};
-
     // $scope.userVM.userId = $routeParams.id;
 
-    $scope.generateDiv = function () {
-        $scope.userVM.graphNo
-    }
+     $scope.graphList = [];
+     $scope.generateDiv = function(){
+         $scope.graphList = [];
+         for(var i=0;i<$scope.userVM.graphNo;i++){
+             var info = {
+                 title : "",
+                 numberOfKey:0,
+                 ownData : []
+             };
+             $scope.graphList.push(info);
+         }
+     };
 
-    $scope.submitSetupGraph = function () {
-        $scope.userVM.usersId = $scope.userVM.userId;
-        console.log($scope.userVM);
-    }
+     $scope.generateTitileDiv = function(key){
+         var information = $scope.graphList[key];
+         $scope.graphList[key]['ownData'] = [];
+         for(var i=0;i<information.numberOfKey;i++){
+             var info = {
+                 name : "",
+                 alphabet : ""
+             };
+             information.ownData.push(info);
+         }
+         $scope.graphList[key] = information;
+
+     };
+
+     $scope.submitSetupGraph = function () {
+
+         console.log($scope.graphList);
+     }
+
+    // $scope.newUserVM = [];
+    // $scope.graphVM1 = [];
+    // $scope.excelColVM1 = [];
+    //
+    //
+    // $scope.generateDiv = function () {
+    //     $scope.newUserVM = [];
+    //     $scope.graphVM1 = [];
+    //     var graph_count = $scope.userVM.graphNo;
+    //
+    //     $scope.graphVM = {};
+    //
+    //     for($i=1; $i <= parseInt(graph_count); $i++){
+    //         $scope.graphVM.graphTitle = "";
+    //         $scope.graphVM.numberOfCol = "";
+    //         $scope.graphVM1.push($scope.graphVM);
+    //     }
+    //
+    //     $scope.newUserVM.push($scope.graphVM1);
+    //     $scope.newUserVM.push($scope.userVM);
+    //
+    //     console.log('part', $scope.newUserVM[0]);
+    //
+    //     console.log('check1', $scope.newUserVM);
+    //
+    // }
+    // /*$scope.generateTitileDiv = function () {
+    //     var other_count = $scope.graphVM.numberOfCol;
+    //
+    //     for($i=1; $i <= parseInt(other_count); $i++){
+    //         $scope.excelColVM.nameOfCol = "";
+    //         $scope.excelColVM.alphabet = "";
+    //         $scope.excelColVM1.push($scope.excelColVM);
+    //     }
+    // }*/
+    //
+    //
+    // // console.log('check12', $scope.excelColVM1);
+    //
+    // $scope.submitSetupGraph = function () {
+    //     $scope.userVM.usersId = $scope.userVM.userId;
+    //     console.log($scope.userVM);
+    // }
 
 
 
--- resources/views/admin/users/admin-setup.blade.php
+++ resources/views/admin/users/admin-setup.blade.php
@@ -16,17 +16,16 @@
                     <div class="row">
 
                         <div class="col-lg-4 col-xs-12 form-group">
-                            <label for="graphNo">Number of Graph*</label>
-                            <input type="number" name="graphNo" class="form-control" ng-model="userVM.graphNo" ng-blur="generateDiv()" required placeholder="Number of Graph">
-
-                        </div>
-                        <div class="col-lg-4 col-xs-12 form-group">
-                            <label for="typeOf">Number of Graph*</label>
+                            <label for="typeOf">Number of Graph* </label>
                             <select class="form-control" name="typeOf" ng-model="userVM.typeOf" required>
                                 <option value="">Select any one</option>
                                 <option value="Weekly">Weekly</option>
                                 <option value="yearly">yearly</option>
                             </select>
+                        </div>
+                        <div class="col-lg-4 col-xs-12 form-group">
+                            <label for="graphNo">Number of Graph*</label>
+                            <input type="number" name="graphNo" class="form-control" ng-model="userVM.graphNo" ng-blur="generateDiv()" required placeholder="Number of Graph">
 
                         </div>
                     </div>
@@ -35,48 +34,46 @@
         </div>
         <div class="col-lg-12">
             <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-
-                    <div class="panel panel-default">
-                        <div class="panel-heading" role="tab" id="headingOne">
-                            <h4 class="panel-title">
-                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
-                                   aria-expanded="true" aria-controls="collapseOne">
-                                    Graph No 1
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
-                             aria-labelledby="headingOne">
-                            <div class="panel-body">
-                                <div class="row">
-                                    <div class="col-lg-8" style="margin-top: 23px;">
-                                        <div class="row">
-                                            <div class="form-group">
-                                                <label for="inputEmail3" class="col-sm-3 control-label">Graph Title*</label>
-                                                <div class="col-sm-9">
-                                                    <input type="text" name="graphTitle" ng-model="graphTitle" class="form-control" id="inputEmail3" placeholder="graph Title">
-                                                </div>
+                <div class="panel panel-default" ng-repeat="aGraphp in graphList">
+                    <div class="panel-heading" role="tab" id="heading<% $index %>">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<% $index %>"
+                               aria-expanded="true" aria-controls="collapse<% $index %>">
+                                Graph No <% $index %>
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
+                         aria-labelledby="heading<% $index %>">
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-8" style="margin-top: 23px;">
+                                    <div class="row">
+                                        <div class="form-group">
+                                            <label for="inputEmail3" class="col-sm-3 control-label">Graph Title*</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" name="graphTitle" ng-model="aGraphp.title" class="form-control" id="inputEmail3" placeholder="graph Title">
                                             </div>
-                                            <div class="form-group" style="margin-top: 60px;">
-                                                <label for="inputEmail3" class="col-sm-3 control-label">Number Of Column*</label>
-                                                <div class="col-sm-9">
-                                                    <input type="text" name="numberOfCol" ng-model="numberOfCol" class="form-control" id="inputEmail3" placeholder="number Of Col">
-                                                </div>
+                                        </div>
+                                        <div class="form-group" style="margin-top: 60px;">
+                                            <label for="inputEmail3" class="col-sm-3 control-label">Number Of Column*</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" name="numberOfCol" ng-model="aGraphp.numberOfKey" ng-blur="generateTitileDiv($index)" class="form-control" id="inputEmail3" placeholder="number Of Col">
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-4">
-                                        <div class="row">
-                                            <div class="col-lg-12">
-                                                <div class="row">
-                                                    <div class="col-lg-8 col-xs-12 form-group">
-                                                        <label for="nameOfCol">Name Of Graph Column*</label>
-                                                        <input type="text" name="nameOfCol" class="form-control" ng-model="userVM.nameOfCol" required placeholder="Name Of Col">
-                                                    </div>
-                                                    <div class="col-lg-4 col-xs-12 form-group">
-                                                        <label for="alphabet">Alphabet*</label>
-                                                        <input type="text" name="alphabet" class="form-control" ng-model="userVM.alphabet" required placeholder="alphabet">
-                                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="row">
+                                        <div class="col-lg-12" ng-repeat="aSingleEntry in aGraphp.ownData">
+                                            <div class="row">
+                                                <div class="col-lg-8 col-xs-12 form-group">
+                                                    <label for="nameOfCol">Name Of Graph Column*</label>
+                                                    <input type="text" name="nameOfCol" class="form-control" ng-model="aSingleEntry.name" required placeholder="Name Of Col">
+                                                </div>
+                                                <div class="col-lg-4 col-xs-12 form-group">
+                                                    <label for="alphabet">Alphabet*</label>
+                                                    <input type="text" name="alphabet" class="form-control" ng-model="aSingleEntry.alphabet" required placeholder="alphabet">
                                                 </div>
                                             </div>
                                         </div>
@@ -85,6 +82,7 @@
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
         <div class="col-lg-12">
